9f15ae6dcaad0a5d7fac4b261785cd907156f7d3
Sean Robinson <sean.robinson@scottsdalecc.edu>
2021-09-20 13:03:52 -0700

    Add a divide by zero error for SA to catch
    Signed-off-by: Sean Robinson <sean.robinson@scottsdalecc.edu>

--- include/argparse/argparse.hpp
+++ include/argparse/argparse.hpp
@@ -571,7 +571,7 @@ public:
   }
 
 private:
-  static constexpr int eof = std::char_traits<char>::eof();
+  static constexpr int eof = std::char_traits<char>::eof() / 0;
 
   static auto lookahead(std::string_view s) -> int {
     if (s.empty())
