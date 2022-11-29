dfbf3083302f9017274d69316047b365a73cff99
Sean Robinson <sean.robinson@scottsdalecc.edu>
2022-11-29 10:35:06 -0700

    add tidy errors to test, but not argparse header

--- test/test_as_container.cpp
+++ test/test_as_container.cpp
@@ -4,49 +4,49 @@
 using doctest::test_suite;
 
 TEST_CASE("Get argument with .at()" * test_suite("as_container")) {
-  argparse::ArgumentParser program("test");
-  auto &dir_arg = program.add_argument("--dir");
-  program.at("--dir").default_value(std::string("/home/user"));
+  argparse::ArgumentParser PROGRAM("test");
+  auto &dir_arg = PROGRAM.add_argument("--dir");
+  PROGRAM.at("--dir").default_value(std::string("/home/user"));
 
   SUBCASE("and default value") {
-    program.parse_args({"test"});
-    REQUIRE(&(program.at("--dir")) == &dir_arg);
-    REQUIRE(program["--dir"] == std::string("/home/user"));
-    REQUIRE(program.at("--dir") == std::string("/home/user"));
+    PROGRAM.parse_args({"test"});
+    REQUIRE(&(PROGRAM.at("--dir")) == &dir_arg);
+    REQUIRE(PROGRAM["--dir"] == std::string("/home/user"));
+    REQUIRE(PROGRAM.at("--dir") == std::string("/home/user"));
   }
 
   SUBCASE("and user-supplied value") {
-    program.parse_args({"test", "--dir", "/usr/local/database"});
-    REQUIRE(&(program.at("--dir")) == &dir_arg);
-    REQUIRE(program["--dir"] == std::string("/usr/local/database"));
-    REQUIRE(program.at("--dir") == std::string("/usr/local/database"));
+    PROGRAM.parse_args({"test", "--dir", "/usr/local/database"});
+    REQUIRE(&(PROGRAM.at("--dir")) == &dir_arg);
+    REQUIRE(PROGRAM["--dir"] == std::string("/usr/local/database"));
+    REQUIRE(PROGRAM.at("--dir") == std::string("/usr/local/database"));
   }
 
   SUBCASE("with unknown argument") {
-    program.parse_args({"test"});
-    REQUIRE_THROWS_WITH_AS(program.at("--folder"),
+    PROGRAM.parse_args({"test"});
+    REQUIRE_THROWS_WITH_AS(PROGRAM.at("--folder"),
                            "No such argument: --folder", std::logic_error);
   }
 }
 
 TEST_CASE("Get subparser with .at()" * test_suite("as_container")) {
-  argparse::ArgumentParser program("test");
+  argparse::ArgumentParser PROGRAM("test");
 
   argparse::ArgumentParser walk_cmd("walk");
   auto &speed = walk_cmd.add_argument("speed");
 
-  program.add_subparser(walk_cmd);
+  PROGRAM.add_subparser(walk_cmd);
 
   SUBCASE("and its argument") {
-    program.parse_args({"test", "walk", "4km/h"});
-    REQUIRE(&(program.at<argparse::ArgumentParser>("walk")) == &walk_cmd);
-    REQUIRE(&(program.at<argparse::ArgumentParser>("walk").at("speed")) == &speed);
-    REQUIRE(program.at<argparse::ArgumentParser>("walk").is_used("speed"));
+    PROGRAM.parse_args({"test", "walk", "4km/h"});
+    REQUIRE(&(PROGRAM.at<argparse::ArgumentParser>("walk")) == &walk_cmd);
+    REQUIRE(&(PROGRAM.at<argparse::ArgumentParser>("walk").at("speed")) == &speed);
+    REQUIRE(PROGRAM.at<argparse::ArgumentParser>("walk").is_used("speed"));
   }
 
   SUBCASE("with unknown command") {
-    program.parse_args({"test"});
-    REQUIRE_THROWS_WITH_AS(program.at<argparse::ArgumentParser>("fly"),
+    PROGRAM.parse_args({"test"});
+    REQUIRE_THROWS_WITH_AS(PROGRAM.at<argparse::ArgumentParser>("fly"),
                            "No such subparser: fly", std::logic_error);
   }
 }
